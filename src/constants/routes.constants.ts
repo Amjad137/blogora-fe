a7215bcb3fee8e5b75e30ab56206c25eb208ca23
amjad137 <mohammedamjath137@gmail.com>
2025-09-08 14:13:18 +0530

    initial commit

---
+++ src/constants/routes.constants.ts
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  ROOT: '/',
+
+  SIGN_UP: '/auth/sign-up',
+  SIGN_IN: '/auth/sign-in',
+  FORGOT_PASSWORD: '/auth/forgot-password',
+  RESET_PASSWORD: '/auth/reset-password',
+
+  USERS_ROOT: '/users', //Access: Admin
+};
+
+export const ADMIN_ROUTES = {
+  ROOT: ROUTES.ROOT,
+  USERS_ROOT: ROUTES.USERS_ROOT,
+};
